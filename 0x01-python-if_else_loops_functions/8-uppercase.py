da9d79364c1ec76bde3fde9ff46dafd8f18533db
Afolabi Temiloluwa <105127078+Temmyaf@users.noreply.github.com>
2022-09-01 15:20:30 +0100

    Update 8-uppercase.py
    correcting error

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,5 +3,7 @@ def uppercase(str):
     for char in str:
         if (ord(char) > 96 and ord(char) < 123):
             char = (chr(ord(char) - 32))
-            print("{}".format(char), end='')
+        else
+            char = char
+    print("{}".format(char), end='')
     print("")
